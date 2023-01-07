578bad0dcd455f8060f0a1607c63f4a52ffb8d23
akshithreddy06 <kattaakshith6@gmail.com>
2023-01-07 23:17:26 +0530

    todo

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -78,7 +78,12 @@ describe("Todo test suite", () => {
     await login(agent, "user.a@test.com", "123456");
     let res = await agent.get("/todos");
     let csrfToken = extractCsrfToken(res);
-  
+    await agent.post("/todos").send({
+      title: "Buy milk",
+      dueDate: new Date().toISOString(),
+      completed: false,
+      _csrf: csrfToken,
+    });
     await agent.post("/todos").send({
       title: "Buy ps3",
       dueDate: new Date().toISOString(),
